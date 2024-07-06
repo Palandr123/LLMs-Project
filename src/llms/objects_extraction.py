fc0b90a413e85ca6880555fa5c49c809bcbfc113
Palandr123 <palandr545@gmail.com>
2024-07-06 09:24:29 +0000

    + object extraction

--- src/llms/objects_extraction.py
+++ src/llms/objects_extraction.py
@@ -4,12 +4,13 @@ from transformers.modeling_utils import PreTrainedModel
 from transformers.tokenization_utils_fast import PreTrainedTokenizerFast
 import torch
 
+from src.llms.prompts import SPOT_OBJECTS_CHAT_TEMPLATE
+
 
 def get_key_objects(
     tokenizer: PreTrainedTokenizerFast,
     model: PreTrainedModel,
     message: str,
-    device: str | torch.device,
     **model_params: dict,
 ) -> dict[str, str | list[tuple[str, list[str | None]]]]:
     """
@@ -19,7 +20,6 @@ def get_key_objects(
         tokenizer (PreTrainedTokenizerFast): The tokenizer used to encode and decode text.
         model (PreTrainedModel): The large language model that generated the response.
         message (str): The message that was used to prompt the model.
-        device (str | torch.device): The device (CPU or GPU) to use for model inference.
         **model_params (dict): Additional parameters to be passed to the model's generate() function.
 
     Returns:
@@ -36,7 +36,7 @@ def get_key_objects(
     )
     input_ids = tokenizer.encode(prompt, add_special_tokens=False, return_tensors="pt")
     generated_ids = model.generate(
-        input_ids=input_ids.to(device),
+        input_ids=input_ids.to(model.device),
         **model_params,
     )
     response = tokenizer.decode(generated_ids[0])
@@ -62,3 +62,34 @@ def get_key_objects(
         "neg_prompt": negative_prompt,
     }
     return parsed_result
+
+
+def spot_objects(
+    tokenizer: PreTrainedTokenizerFast,
+    model: PreTrainedModel,
+    prompt: str,
+    **model_params: dict,
+) -> dict[str, str | list[tuple[str, list[str | None]]]]:
+    """
+    This function identifies objects mentioned in a given prompt using a pre-trained 
+    tokenizer and model.
+
+    Args:
+        tokenizer (PreTrainedTokenizerFast): The tokenizer to use for processing the text.
+        model (PreTrainedModel): The pre-trained model used for object detection.
+        prompt (str): The user prompt containing the text to analyze for objects.
+        **model_params (dict): Additional parameters to pass to the pre-trained model.
+
+    Returns:
+        dict[str, str | list[tuple[str, list[str | None]]]]: A dictionary containing:
+            * objects (list[tuple[str, list[str | None]]]): A list of tuples where the first
+                element is the object name and the second element is a list of attributes or None
+                if not available.
+            * bg_prompt (str): The background prompt extracted from the response.
+            * neg_prompt (str): The negation prompt extracted from the response (empty string if
+                not present).
+    """
+    question = {"role": "user", "content": f"User Prompt: {prompt}"}
+    message = [*SPOT_OBJECTS_CHAT_TEMPLATE, question]
+    result = get_key_objects(tokenizer, model, message, **model_params)
+    return result
